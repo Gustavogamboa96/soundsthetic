8c225ee180a34350e7b8d5325325ab2a92d963af
Gustavogamboa96 <Gustavo.gamboa96@gmail.com>
2022-08-19 15:36:05 -0400

    Update to netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 /** @type {import('@sveltejs/kit').Config} */
-import vercel from '@sveltejs/adapter-vercel';
+import netlify from '@sveltejs/adapter-netlify@next';
 
 export default{
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: vercel()
+		adapter: netlify()
 	}
 };
 
